78d46623d956d883154e1949006daf85489694bd
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-04-18 13:35:24 -0500

    added SaveButton

---
+++ client/src/components/SaveButton/Savebutton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SaveButton() {
+    return (
+        <button type="button" className="btn" style = {{float: 'right'}}>
+        </button>
+    )
+}
+export default SaveButton;
