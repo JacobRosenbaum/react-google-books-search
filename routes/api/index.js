75e90f00b815fbeeb3576bbdb3662985dac276a1
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-04-18 13:57:34 -0500

    fixed routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const bookRoutes = require("./api");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/api", bookRoutes);
 
 module.exports = router;
