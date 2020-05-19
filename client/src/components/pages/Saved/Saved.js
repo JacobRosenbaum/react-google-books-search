08f75c102ef49266a3feddd8b0fef02daf3a445e
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-05-19 10:24:49 -0500

    updated save page styling

--- client/src/components/pages/Saved/Saved.js
+++ client/src/components/pages/Saved/Saved.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import Header from "../../../components/Header/Header";
 import SaveBox from "../../SaveBox/SaveBox";
 import TitleTron from "../../TitleTron/TitleTron";
+import "./saved.css"
 
 function Search() {
     return (
-        <div>
-            <Header />
-            <TitleTron />
-            <SaveBox />
+        <div class="saveContainer">
+            <div class="saveHeader">
+                <Header />
+            </div>
+            <div class="saveBody">
+                <TitleTron />
+                <SaveBox />
+            </div>
         </div>
     )
 }
