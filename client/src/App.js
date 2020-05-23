816bad1c1e41e42f52c7706807e93941a001f6fb
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-05-23 12:41:39 -0500

    deploying to Heroku to test

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Header/Header"
 import SearchBox from "../src/components/SearchBox/SearchBox";
 import Search from "../src/components/pages/Search/Search";
-import Saved from "../src/components/pages/Saved/Saved"
+import Saved from "../src/components/pages/Saved/Saved";
+import SaveBox from "../src/components/SaveBox/SaveBox"
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       <Router>
         <div>
           <Route exact path="/" component={Search} />
-          <Route exact path="/saved" component={Saved} />
+          <Route exact path="/saved" component={SaveBox} />
         </div>
       </Router>
       </div>
--- client/src/components/pages/Saved/Saved.js
+++ client/src/components/pages/Saved/Saved.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../../components/Header/Header";
 import SaveBox from "../../SaveBox/SaveBox";
 import TitleTron from "../../TitleTron/TitleTron";
